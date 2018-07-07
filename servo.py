9d7f465ec3d6db20446b2ef27e3e255fec15dcd9
Mohi Pi <mohi.khansari@gmail.com>
2018-07-07 04:36:19 +0000

    Testing servo.py on the hardware and fixing some typos.

--- servo.py
+++ servo.py
@@ -1,19 +1,23 @@
+import numpy as np
 import pigpio
 import RPi.GPIO as GPIO
 import time
 
-MotorParameters = namedtuple('MotorParameters', 
-                             'pin, pulse_range, angle_range')
+class MotorParameters(object):
+  def __init__(self, pin, pulse_range, angle_range):
+    self.pin = pin
+    self.pulse_range = pulse_range
+    self.angle_range = angle_range
 
 
 MOTOR_PARAMETERS = {
-  'pitch': MotorParameters(pin=19, pulse_range=(2500, 1500), angle_range=(0, 90),
+  'pitch': MotorParameters(pin=19, pulse_range=(2500, 1500), angle_range=(0, 90)),
   'yaw': MotorParameters(pin=13, pulse_range=(500, 1500), angle_range=(0, 90)),
 }
 
 
 def AngleToPulseWidth(angle, motor_parameter):
-  return np.interp1(angle, motor_parameter.angle_range, motor_parameter.pulse_range)
+  return int(np.interp(angle, motor_parameter.angle_range, motor_parameter.pulse_range))
 
 JOINTS_PIN = [19, 26]
 BUTTONS_PIN = [18, 23]
@@ -32,9 +36,10 @@ try:
         print 'Button %d pressed' % button_pin
     time.sleep(0.2)
     angle = int(raw_input('write and angle: '))
-    AngleToPulseWidth(angle, MOTOR_PARAMETERS['pitch'])
-    AngleToPulseWidth(angle, MOTOR_PARAMETERS['yaw'])
-    continue
+    for angle_name in ['pitch', 'yaw']:
+      pulse_width = AngleToPulseWidth(angle, MOTOR_PARAMETERS[angle_name])
+      motor_control.set_servo_pulsewidth(
+          MOTOR_PARAMETERS[angle_name].pin, pulse_width)   
 except KeyboardInterrupt:
   GPIO.output(BUTTON_PIN_POWER, False)
   for joint in joints:
